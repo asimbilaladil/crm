db95102db90738265b45d1fa1ddbd58d8d82f590
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-09-14 15:17:28 +0500

    add employee

---
+++ application/views/AddEmployee.php
@@ -0,0 +1,93 @@
+<ul class="breadcrumb">
+    <li><a href="#">Home</a></li>
+    <li class="active">Add Employee</li>
+</ul>
+<!-- PAGE CONTENT WRAPPER -->
+<div class="page-content-wrap">
+                
+                    <div class="row">
+                        <div class="col-md-12">
+                            
+                            <form class="form-horizontal">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title"><strong>Add</strong> Employee</h3>
+                                  
+                                </div>
+                                <div class="panel-body">
+                                    <p> Add your company employee </p>
+                                </div>
+                                <div class="panel-body">                                                                        
+                                    
+                                    <div class="form-group">
+                                        <label class="col-md-3 col-xs-12 control-label">First Name</label>
+                                        <div class="col-md-6 col-xs-12">                                            
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><span class="fa fa-user"></span></span>
+                                                <input type="text" class="form-control" name="firstName">
+                                            </div>                                            
+
+                                        </div>
+                                    </div>
+                                   <div class="form-group">
+                                        <label class="col-md-3 col-xs-12 control-label">Last Name</label>
+                                        <div class="col-md-6 col-xs-12">                                            
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><span class="fa fa-user"></span></span>
+                                                <input type="text" class="form-control" name="lastName">
+                                            </div>                                            
+
+                                        </div>
+                                    </div>
+                                    <div class="form-group">                                        
+                                        <label class="col-md-3 col-xs-12 control-label">Email</label>
+                                        <div class="col-md-6 col-xs-12">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><span class="fa fa-envelope-o"></span></span>
+                                                <input type="email" class="form-control" name="email">
+                                            </div>            
+
+                                        </div>
+                                    </div>                                    
+                                    <div class="form-group">                                        
+                                        <label class="col-md-3 col-xs-12 control-label">Password</label>
+                                        <div class="col-md-6 col-xs-12">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><span class="fa fa-unlock-alt"></span></span>
+                                                <input type="password" class="form-control" name="password">
+                                            </div>            
+                                        </div>
+                                    </div>
+                                <div class="form-group">
+                                        <label class="col-md-3 col-xs-12 control-label">Update Password</label>
+                                        <div class="col-md-6 col-xs-12">                                            
+                                            <div class="checkbox">
+                                                <label><input type="checkbox" value="">Ask User to change password</label>
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="form-group">
+                                        <label class="col-md-3 col-xs-12 control-label">Permissions</label>
+                                        <div class="col-md-6 col-xs-12">                                            
+                                            <div class="checkbox">
+                                                <label> <input type="checkbox" value="">Add / Update </label> &nbsp;    
+                                                <label> <input type="checkbox" value=""> View </label> &nbsp;   
+                                                <label> <input type="checkbox" value=""> Delete </label>&nbsp;  
+                                            </div>
+
+                                        </div>
+                                </div>                              
+
+                                </div>
+                                <div class="panel-footer">
+                                    
+                                    <button class="btn btn-primary pull-right">Submit</button>
+                                </div>
+                            </div>
+                            </form>
+                            
+                        </div>
+                    </div>                    
+                    
+</div>
+<!-- END PAGE CONTENT WRAPPER -->
