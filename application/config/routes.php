fb2191e2c1215a647a18ab2ecf5dd90dff04a296
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-10-23 16:08:29 +0500

    add client portal

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'home';
+$route['client/login/(:any)'] = 'Client-Portal/Login/index/$id';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
