11f5745621a27cc0b9364a1bda29babc1bd1be54
asimbilaladil <asimbilal@Asims-MacBook.local>
2016-10-29 01:47:12 +0500

    fix design

--- application/views/Questionnaire/QuestionnaireAttempt.php
+++ application/views/Questionnaire/QuestionnaireAttempt.php
@@ -41,7 +41,7 @@
                                 <label class="col-md-3 col-xs-12 control-label"> Question '. $count .': </label>
                                 <div class="col-md-6 col-xs-12">
                                     <div class="input-group">
-                                         <p style="padding-top: 7px;"  class="col-md-6 col-xs-12" >  '. $question['question'] .' </p>
+                                         <h4 style="padding-top: 7px;"  class="col-md-12 col-xs-12" >  '. $question['question'] .' </h4>
                                          <input type="hidden" name="questionId_'. $count .'" value="'. $question['question_id'] .'" />
                                          <input type="hidden" name="totalQuestions" value="'. $count .'" />
                                          <input type="hidden" name="questionType_'. $count .'" value="'. $question['type'] .'" />
@@ -54,13 +54,13 @@
                             if ($question['type'] == 'multi') {
                                 foreach ($question['multiQuestions'] as $item) {
 
-                                    echo ' <div class="col-md-3 col-xs-3"></div><div class="form-group col-md-9 col-xs-9"> <input class="iradio" type="radio" name="answer_'. $count .'" value="'. $item .'"> &nbsp;&nbsp;'. $item .' </input> </div>';
+                                    echo ' <div class="col-md-3 col-xs-3"></div><div style="margin-left: 15px;" class="form-group col-md-9 col-xs-9"> <input class="iradio" type="radio" name="answer_'. $count .'" value="'. $item .'"> &nbsp;&nbsp;'. $item .' </input> </div>';
 
                                 }
                             //text
                             } else {
 
-                                echo '<div class="col-md-3 col-xs-3"></div><div class="form-group col-md-6 col-xs-12"> <input  style="width: 50%;" type="text" name="answer_'. $count .'" /></div>';
+                                echo '<div class="col-md-3 col-xs-3"></div><div style="margin-left: 15px;" class="form-group col-md-6 col-xs-12"> <input  style="width: 50%;" type="text" name="answer_'. $count .'" /></div>';
 
                             }
                         }
